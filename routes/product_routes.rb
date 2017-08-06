533c9c1638149bed7e19b874a80e3517e0ecefba
nak1114 <naktak1114@gmail.com>
2017-08-07 03:00:42 +0900

    Add editor page

--- routes/product_routes.rb
+++ routes/product_routes.rb
@@ -29,66 +29,125 @@ class MyAppRoute::Product < Sinatra::Base
 
 
   get '/pdf' do
-    #@pages=TrList.all.limit(10)
-    ActiveRecord::Base.connection_pool.with_connection do
-      @statuses=Status.all
-      @pages=Product.includes(:status).search(params[:keyword]).sfilter(params[:status]).order(:updated_at).reverse_order.paginate(:page => params[:page], :per_page => 5)
-      slim :pdf
-    end
+    @statuses=Status.all
+    @pages=Product.includes(:status).search(params[:keyword]).sfilter(params[:status]).order(:updated_at).reverse_order.paginate(:page => params[:page], :per_page => 50)
+    slim :pdf
   end
 
   post '/pdf' do
     body = request.body.read
     params= JSON.parse body
-    #ActiveRecord::Base.connection.close
-    ActiveRecord::Base.connection_pool.with_connection do
-      if Product.exists?(url: params['url'])
-        return {
-          warn: '重複',
-          status: 'URL重複',
-          title: params['name'],
-          url: params['url'],
-        }.to_json
-      end
-      name=params['name'].gsub(/[[:cntrl:]]/, '').to_fname.strip
-      #name=params['name'].to_fname
-
-      dup=Product.where(name: name).count
-      #id=params['url'].sub(%r!\A.*/item/(.*)\z!,"\\1")
-      pd=Product.new
-      pd.url =params['url']
-      pd.name=name
-      pd.status_id=1
-      
-      json = if dup>0
-        params['rename']=name+(dup+1).to_s
-        pd.rename=params['rename']
-        name=params['rename']
-        {
-          warn: '重複',
-          status: '重複&リネーム',
-          title: name,
-          url: params['url'],
-        }
-      else
-        {
-          status: '下戴',
-          title: name,
-          url: params['url'],
-        }
-      end
-      ret=dl(params['url'],name)
-      if ret["success"]
-        pd.save
-        return json.to_json
-      end
+    if Product.exists?(url: params['url'])
       return {
-        warn: '無効',
-        status: 'StationError',
-        title: name,
+        warn: '重複',
+        status: 'URL重複',
+        title: params['name'],
         url: params['url'],
       }.to_json
     end
+    name=params['name'].to_fname
+
+    dup=Product.where(name: name).count
+    #id=params['url'].sub(%r!\A.*/item/(.*)\z!,"\\1")
+    pd=Product.new
+    pd.url =params['url']
+    pd.name=name
+    pd.status_id=1
+    
+    json = if dup>0
+      params['rename']=name+(dup+1).to_s
+      pd.rename=params['rename']
+      name=params['rename']
+      {
+        warn: '重複',
+        status: '重複&リネーム',
+        title: name,
+        url: params['url'],
+      }
+    else
+      {
+        status: '下戴',
+        title: name,
+        url: params['url'],
+      }
+    end
+    ret=dl(params['url'],name)
+    if ret["success"]
+      pd.save
+      return json.to_json
+    end
+    return {
+      warn: '無効',
+      status: 'StationError',
+      title: name,
+      url: params['url'],
+    }.to_json
+  end
+
+  post '/pdf/:id/reload' do |id|
+    @page=Product.find(id.to_i)
+
+    name=@page.rename
+    name=@page.name if name==nil || name==''
+    name=name.to_fname
+    ret=dl(@page.url,name)
+    if ret["success"]
+      @page.update({status_id: 1})
+      return {
+        message: '完了！'+name,
+        rename: name
+      }.to_json
+    else
+      @page.update({status_id: 2})
+      return {
+        error:  'エラー',
+        message: 'Stationエラー',
+      }.to_json
+    end
+  end
+
+
+  get '/pdf/:id' do |id|
+    @statuses=Status.all
+    @page=Product.find(id.to_i)
+    slim :pdf_id
+  end
+
+  post '/pdf/:id' do |id|
+    @page=Product.find(id.to_i)
+    @statuses=Status.all
+    colums={}
+    colums[:name]  =params["name"].to_fname   if params["name"]   && params["name"].size >2
+    colums[:rename]=params["rename"].to_fname if params["rename"] && params["rename"].size >2
+    colums[:status_id]=params["status"].to_i
+    @page.update(colums)
+
+    if params["dl"]=='on' && (colums[:status_id]==4 ||colums[:status_id]==5 )
+      name=@page.rename
+      name=@page.name if name==nil || name==''
+      name=name.to_fname
+      ret=dl(page.url,name)
+      if ret["success"]
+        @page.update({status_id: 1})
+      else
+        @page.update({status_id: 2})
+      end
+    end
+    slim :pdf_id
+  end
+
+  delete '/pdf/:id' do |id|
+    body = request.body.read
+    json = JSON.parse body
+    pp params
+    pp json
+    {
+      message: 'complete!'
+    }.to_json
+  end
+
+  after do
+    ActiveRecord::Base.connection.close
   end
 end
 
--- routes/tr_list_routes.rb
+++ routes/tr_list_routes.rb
@@ -27,122 +27,137 @@ class MyAppRoute::TrList < Sinatra::Base
     register Sinatra::Reloader
   end
 
+  get '/tr' do
+    @statuses=Status.all
+    @pages=TrList.includes(:status).search(params[:keyword]).sfilter(params[:status]).order(:updated_at).reverse_order.paginate(:page => params[:page], :per_page => 50)
+    slim :tr
+  end
+
   post '/tr' do
-    ActiveRecord::Base.connection_pool.with_connection do
-      body = request.body.read
-      params= JSON.parse body
-      if TrList.exists?(url: params['url'])
-        return {
-          warn: '重複',
-          status: 'URL重複',
-          title: params['name'],
-          url: params['url'],
-        }.to_json
-      end
-      ret=dl_tr(params['url'])
+    body = request.body.read
+    params= JSON.parse body
+    if TrList.exists?(url: params['url'])
+      return {
+        warn: '重複',
+        status: 'URL重複',
+        title: params['name'],
+        url: params['url'],
+      }.to_json
+    end
+    ret=dl_tr(params['url'])
+    columns={}
+    columns[:url]=params['url']
+    columns[:name]=ret[:code] || ret[:ename]
+    columns[:rename]=ret[:jname] if ret[:jname]
+    columns[:created_at]=ret[:utile] if ret[:utile]
+    case ret[:action]
+    when :site_err
+      TrList.create(columns.merge({status_id: 2}))
+      return {
+        warn: 'エラー',
+        status: 'Site_エラー:' + ret[:code],
+        title: params['name'],
+        url: params['url'],
+      }.to_json
+    when :no_name
+      TrList.create(columns.merge({status_id: 4}))
+      return {
+        warn: 'エラー',
+        status: '日本語タイトルなし',
+        title: ret[:ename],
+        url: params['url'],
+      }.to_json
+    when :dup
+      TrList.create(columns.merge({status_id: 5}))
+      return {
+        warn: 'エラー',
+        status: '重複タイトル',
+        title: ret[:ename],
+        url: params['url'],
+      }.to_json
+    when :ok
+      TrList.create(columns.merge({status_id: 1}))
+      return {
+        status: '下戴',
+        title: ret[:ename],
+        url: params['url'],
+      }.to_json
+    end
+  end
+
+  post '/tr/:id/reload' do |id|
+    page=TrList.find(id.to_i)
+    #page=TrList.find(params['id'].to_i)
+    ret=dl_tr(page.url)
+    columns={}
+    columns[:name]=ret[:code] || ret[:ename]
+    columns[:rename]=ret[:jname] if ret[:jname]
+    case ret[:action]
+    when :site_err
+      page.update(colums.merge({status_id: 2}))
+      return {
+        error:  'エラー',
+        message: 'サイトエラー;'+ret[:code],
+      }.to_json
+    when :no_name
+      page.update(colums.merge({status_id: 4}))
+      return {
+        error:  'エラー',
+        message: '名前が無効:'+ret[:jname],
+      }.to_json
+    when :dup
+      page.update(colums.merge({status_id: 5}))
+      return {
+        error:  'エラー',
+        message: '同名ファイルがあるため更新だけ完了、ダウンロード未完了;'+ret[:jname],
+        rename: ret[:jname]
+      }.to_json
+    when :ok
+      page.update(colums.merge({status_id: 1}))
+      return {
+        message: '完了！'+ret[:jname],
+        rename: ret[:jname]
+      }.to_json
+    end
+    return {
+      error:  'エラー',
+      message: '不明なエラー！',
+    }.to_json
+  end
+
+  get '/tr/:id' do |id|
+    @statuses=Status.all
+    @page=TrList.find(id.to_i)
+    slim :tr_id
+  end
+  
+  post '/tr/:id' do |id|
+    @page=TrList.find(id.to_i)
+    @statuses=Status.all
+    colums={}
+    colums[:name]  =params["name"]   if params["name"]   && params["name"].size >2
+    colums[:rename]=params["rename"] if params["rename"] && params["rename"].size >2
+    colums[:status_id]=params["status"].to_i
+    @page.update(colums)
+
+    if params["dl"]=='on' && (colums[:status_id]==4 ||colums[:status_id]==5 )&& params["rename"].size >5
+      ret=dl_tr(@page.url,@page.rename)
+      page=@page
       case ret[:action]
       when :site_err
-        TrList.create({
-          url: params['url'],
+        page.update({
           name: ret[:code],
           status_id: 2,
         })
-        return {
-          warn: 'エラー',
-          status: 'Site_エラー:' + ret[:code],
-          title: params['name'],
-          url: params['url'],
-        }.to_json
       when :no_name
-        TrList.create({
-          url: params['url'],
-          name: ret[:ename],
-          status_id: 4,
-          created_at: ret[:utime],
-        })
-        return {
-          warn: 'エラー',
-          status: '日本語タイトルなし',
-          title: ret[:ename],
-          url: params['url'],
-        }.to_json
+        page.update({status_id: 4})
       when :dup
-        TrList.create({
-          url: params['url'],
-          name: ret[:ename],
-          rename: ret[:jname],
-          status_id: 5,
-          created_at: ret[:utime],
-        })
-        return {
-          warn: 'エラー',
-          status: '重複タイトル',
-          title: ret[:ename],
-          url: params['url'],
-        }.to_json
+        page.update({status_id: 5})
       when :ok
-        TrList.create({
-          url: params['url'],
-          name: ret[:ename],
-          rename: ret[:jname],
-          status_id: 1,
-          created_at: ret[:utime],
-        })
-        return {
-          status: '下戴',
-          title: ret[:ename],
-          url: params['url'],
-        }.to_json
+        page.update({status_id: 1})
       end
     end
-  end
-
-  end
-
-  put '/tr/:id' do
-    ActiveRecord::Base.connection_pool.with_connection do
-      body = request.body.read
-      json = JSON.parse body
-      pp params
-      pp json
-      page=TrList.find(params['id'].to_i)
-      json['url']
-      ret=dl_tr(json['url'],json['name'])
-      case ret[:action]
-      when :site_err
-        return {
-          error:  'エラー',
-          message: 'サイトエラー;'+ret[:code],
-        }.to_json
-      when :no_name
-        return {
-          error:  'エラー',
-          message: '名前が無効:'+ret[:jname],
-        }.to_json
-      when :dup
-        page.update({
-          rename: ret[:jname],
-        })
-        return {
-          error:  'エラー',
-          message: '同名ファイルがあるため更新だけ完了、ダウンロード未完了;'+ret[:jname],
-          rename: ret[:jname]
-        }.to_json
-      when :ok
-        page.update({
-          rename: ret[:jname],
-          status_id: 1,
-        })
-        return {
-          message: '完了！'+ret[:jname],
-          rename: ret[:jname]
-        }.to_json
-      end
-      return {
-        error:  'エラー',
-        message: '不明なエラー！',
-      }.to_json
-    end
+    slim :tr_id
   end
 
   delete '/tr/:id' do
@@ -155,13 +170,8 @@ class MyAppRoute::TrList < Sinatra::Base
     }.to_json
   end
 
-  get '/tr' do
-    #@pages=TrList.all.limit(10)
-    ActiveRecord::Base.connection_pool.with_connection do
-      @statuses=Status.all
-      @pages=TrList.includes(:status).search(params[:keyword]).sfilter(params[:status]).order(:updated_at).reverse_order.paginate(:page => params[:page], :per_page => 5)
-      slim :tr
-    end
+  after do
+    ActiveRecord::Base.connection.close
   end
 
 end
